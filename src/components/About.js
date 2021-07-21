61ee326dd8fc1a89f7e4ba31fb173d9fc5ca8805
Rosa <rosarenee@att.net>
2021-07-21 08:23:52 -0500

    commit tree 3632b8ff0ffdb46139788bc62773132a525fdc99

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,17 @@
 import React from "react";
 
-function About() {
+function About(props) {
   return (
     <div id="about">
-      <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <h2>About Me</h2>      
+      <p>{props.bio}</p>
+
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      
       {/* add your <Links /> component here */}
+      <h2>Links</h2>
+      <a href="git">{props.github}</a>
+      <a href="link">{props.linkedin}</a>
     </div>
   );
 }
---
+++ src/components/Links.js
@@ -0,0 +1,8 @@
+const Links = {
+
+      github: "https://github.com/liza",
+      linkedin: "https://www.linkedin.com/in/liza/",
+    
+  };
+  
+  export default Links;
